c5109c92fa925babd76f050aaaae333818834db4
AzirEmperorShurima <tranvantri352@gmail.com>
2025-04-25 19:11:24 +0700

    refactor(auth): enhance password validation middleware and update email templates
    Improve the password validation middleware to handle multiple password fields and add enhanced email templates for better user experience. The middleware now checks for password strength across various fields, ensuring consistency and security. Email templates have been updated with a modern design and additional branding elements.

--- ProG_BE/src/models/BlackList.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const MailBlacklistSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    reason: { type: String, required: true },  // Lý do bị cấm
-    addedAt: { type: Date, default: Date.now }
-});
-
-export default mongoose.model("MailBlacklist", MailBlacklistSchema)
